de8da43aca2f3d61b556e7115b7b1bc18f434841
Agustin Lara <lara.agustin.c@gmail.com>
2022-12-06 18:35:11 -0300

    Optimizar diseño sección 'sobre mi'
    Añadir animación al scrollear

--- main.js
+++ main.js
@@ -2,6 +2,24 @@
 
 window.onload = () => {
 
-    
+    /*
+    ===============  |SCROLL REVEAL|  ================
+    ==================================================
+    */
+    let revealList = document.querySelectorAll('.reveal');
+    let revealCount = 0;
+    function reveal(){
+        if (revealCount < revealList.length){
+            if (150 < window.innerHeight - revealList[revealCount].getBoundingClientRect().top){
+                revealList[revealCount].classList.add('active');
+                revealCount++;
+            }
+        }
+    }
+
+    for(let i = 0; i < revealList.length; i++){
+        reveal();
+    }
+    window.addEventListener('scroll', reveal);
 
 }
