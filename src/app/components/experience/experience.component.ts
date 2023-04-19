8bd2cbcc31bf60268f39067b5d93358399e77802
Agustin Lara <lara.agustin.c@gmail.com>
2023-04-19 20:05:22 -0300

    Iniciar diseño y funcionamiento Experiencia

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Experience } from 'src/app/models/Experience';
+import { PortfolioService } from 'src/app/portfolio/portfolio.service';
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.css']
 })
-export class ExperienceComponent {
+export class ExperienceComponent implements OnInit {
+
+  constructor(private portfolioService:PortfolioService) {}
+
+  experiences!: Experience[];
+
+  ngOnInit(): void {
+    this.portfolioService.getExperience().subscribe(response => {
+      this.experiences = response;
+    });
+  }
 
 }
