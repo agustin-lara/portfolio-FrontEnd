8526e3afc1b3edaa2cd1232f44b4e9867c6b2981
Agustin Lara <lara.agustin.c@gmail.com>
2023-05-06 16:43:37 -0300

    Implementar funcionamiento completo Projects

---
+++ src/app/services/projects.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Project } from '../models/Project';
+import { environment } from 'src/environments/environment.development';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectsService {
+
+  constructor(private http:HttpClient) { }
+
+  projectRequest: Object | undefined;
+
+  getProjects(): Observable<Project[]> {
+    return this.http.get<Project[]>(environment.apiUrl + "/api/proyectos/1");
+  }
+
+  addProject(project: Project) {
+    this.projectRequest = {
+      nombre: project.nombre,
+      descripcion: project.descripcion,
+      tecnologias: project.tecnologias,
+      imagen: project.imagen,
+      perfilId: 1
+    };
+    return this.http.post(environment.apiUrl + "/api/proyectos", this.projectRequest, {responseType: 'text'});
+  }
+
+  updateProject(project: Project) {
+    this.projectRequest = {
+      nombre: project.nombre,
+      descripcion: project.descripcion,
+      tecnologias: project.tecnologias,
+      imagen: project.imagen,
+      perfilId: 1
+    };
+    return this.http.put(environment.apiUrl + "/api/proyectos/" + project.id, this.projectRequest, {responseType: 'text'});
+  }
+
+  deleteProject(id: number) {
+    return this.http.delete(environment.apiUrl + "/api/proyectos/" + id, {responseType: 'text'});
+  }
+
+}
