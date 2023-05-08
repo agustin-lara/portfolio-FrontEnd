8e5f473b66547c44797caa16cd9b7ddc5feef4db
Agustin Lara <lara.agustin.c@gmail.com>
2023-05-08 11:22:39 -0300

    Implementar funcionamiento completo Skills

--- src/app/models/Hardskill.ts
+++ src/app/models/Hardskill.ts
@@ -1,16 +1,6 @@
-export class Hardskill {
+export interface Hardskill {
     id: number;
     nombre: string;
     dominio: number;
-    imagen: string;
     color: string;
-
-    constructor(id=0,nombre="",dominio=0,imagen="",color="") {
-        this.id = id;
-        this.nombre = nombre;
-        this.dominio = dominio;
-        this.imagen = imagen;
-        this.color = color;
-    }
-
 }
--- src/app/services/skills.service.ts
+++ src/app/services/skills.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Softskill } from '../models/Softskill';
+import { environment } from 'src/environments/environment.development';
+import { Hardskill } from '../models/Hardskill';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +12,66 @@ export class SkillsService {
 
   constructor(private http:HttpClient) { }
 
-  getSoftskills(): Observable<any> {
-    return this.http.get("assets/data/softskills.json");
+  softskillRequest: Object | undefined;
+  hardskillRequest: Object | undefined;
+
+  // SOFTSKILLS
+  getSoftskills(): Observable<Softskill[]> {
+    return this.http.get<Softskill[]>(environment.apiUrl + "/api/habilidades-blandas/1");
   }
 
-  getHardskills(): Observable<any> {
-    return this.http.get("assets/data/hardskills.json");
+  addSoftskill(softskill: Softskill) {
+    this.softskillRequest = {
+      nombre: softskill.nombre,
+      descripcion: softskill.descripcion,
+      imagen: softskill.imagen,
+      perfilId: 1
+    };
+    return this.http.post(environment.apiUrl + "/api/habilidades-blandas", this.softskillRequest, {responseType: 'text'});
+  }
+
+  updateSoftskill(softskill: Softskill) {
+    this.softskillRequest = {
+      nombre: softskill.nombre,
+      descripcion: softskill.descripcion,
+      imagen: softskill.imagen,
+      perfilId: 1
+    };
+    return this.http.put(environment.apiUrl + "/api/habilidades-blandas/" + softskill.id, this.softskillRequest, {responseType: 'text'});
+  }
+
+  deleteSoftskill(id: number) {
+    return this.http.delete(environment.apiUrl + "/api/habilidades-blandas/" + id, {responseType: 'text'});
+  }
+
+
+  // HARDSKILLS
+  getHardskills(): Observable<Hardskill[]> {
+    return this.http.get<Hardskill[]>(environment.apiUrl + "/api/habilidades-duras/1");
+  }
+
+  addHardskill(hardskill: Hardskill) {
+    this.hardskillRequest = {
+      nombre: hardskill.nombre,
+      dominio: hardskill.dominio,
+      color: hardskill.color,
+      perfilId: 1
+    };
+    return this.http.post(environment.apiUrl + "/api/habilidades-duras", this.hardskillRequest, {responseType: 'text'});
+  }
+
+  updateHardskill(hardskill: Hardskill) {
+    this.hardskillRequest = {
+      nombre: hardskill.nombre,
+      dominio: hardskill.dominio,
+      color: hardskill.color,
+      perfilId: 1
+    };
+    return this.http.put(environment.apiUrl + "/api/habilidades-duras/" + hardskill.id, this.hardskillRequest, {responseType: 'text'});
+  }
+
+  deleteHardskill(id: number) {
+    return this.http.delete(environment.apiUrl + "/api/habilidades-duras/" + id, {responseType: 'text'});
   }
 
 }
