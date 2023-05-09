0e06166a1184d6eda86aa10317506ab3bd2e2622
Agustin Lara <lara.agustin.c@gmail.com>
2023-05-09 19:18:13 -0300

    Implementar funcionamiento completo Experiencia

--- src/app/services/experience.service.ts
+++ src/app/services/experience.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+import { Experience } from '../models/Experience';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +11,36 @@ export class ExperienceService {
 
   constructor(private http:HttpClient) { }
 
-  getExperiences(): Observable<any> {
-    return this.http.get("assets/data/experiences.json");
+  experienceRequest: Object | undefined;
+
+  getExperiences(): Observable<Experience[]> {
+    return this.http.get<Experience[]>(environment.apiUrl + "/api/empleos/1");
+  }
+
+  addExperience(experience: Experience) {
+    this.experienceRequest = {
+      nombre: experience.nombre,
+      institucion: experience.institucion,
+      fecha: experience.fecha,
+      imagen: experience.imagen,
+      perfilId: 1
+    };
+    return this.http.post(environment.apiUrl + "/api/empleos", this.experienceRequest, {responseType: 'text'});
+  }
+
+  updateExperience(experience: Experience) {
+    this.experienceRequest = {
+      nombre: experience.nombre,
+      institucion: experience.institucion,
+      fecha: experience.fecha,
+      imagen: experience.imagen,
+      perfilId: 1
+    };
+    return this.http.put(environment.apiUrl + "/api/empleos/" + experience.id, this.experienceRequest, {responseType: 'text'});
+  }
+
+  deleteExperience(id: number) {
+    return this.http.delete(environment.apiUrl + "/api/empleos/" + id, {responseType: 'text'});
   }
 
 }
